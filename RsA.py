b02f70dd74dc4c7dbdd2eba7fdf4caf0f76eb929
JohnV <johnv@wenca.cz>
2022-09-25 16:58:31 +0200

    Added testing runs, DEBUG guard test

--- RsA.py
+++ RsA.py
@@ -1,11 +1,14 @@
 #Author: Jan Vašák, 24.9.2022
 
+
+
+
 class Transition:
     orig = ''
     symbol = ''
-    eqGuard = {}
-    diseqGuard = {}
-    update = {}
+    eqGuard = set()
+    diseqGuard = set()
+    update = set()      #{('r1', 5), ...}
     dest = ''
     def __init__(self, orig, symbol, eqGuard, diseqGuard, update, dest):
         self.orig = orig
@@ -16,11 +19,11 @@ class Transition:
         self.dest = dest
 
 class RsA:
-    Q = {}          #set of states
-    R = {}          #set of registers
-    delta = {}      #set of transitions 
-    I = {}          #set of initial states
-    F = {}          #set of final states
+    Q = set()          #set of states
+    R = list()             #list(python doesn't have nested sets?) of registers {([r1', {5, 4, 1}], ...}
+    delta = set()      #set of transitions 
+    I = set()          #set of initial states
+    F = set()          #set of final states
 
     def __init__(self, Q, R, delta, I, F):
         self.Q = Q
@@ -29,34 +32,84 @@ class RsA:
         self.I = I
         self.F = F
 
+    #up shouldn't update the same register multiple times with different values
+    def updateRegs(self, up):
+        for val in up:
+            for r in self.R: 
+                if r[0] == val[0]:
+                    r[1].add(val[1])
+
+    def clearRegs(self):
+        for r in self.R:
+            r[1] = set()
+
+    def guardTest(self, val, eqG, diseqG):
+        #(self.guardTest(i[1], t.eqGuard) or t.eqGuard == set()) and (t.diseqGuard == set() or not self.guardTest(i[1], t.diseqGuard))
+        #is there a register in the guard that does not have in (val) as it's element?
+        for g in eqG:
+            for r in self.R:
+                if g == r[0]:
+                    if not {val}.issubset(r[1]):
+                        return False
+
+        for g in diseqG:
+            for r in self.R:
+                if g == r[0]:
+                    if {val}.issubset(r[1]):
+                        return False
+        return True
+
     def runWord(self, word):
         print("This would run", word, "over this RsA")
 
-    """
-    #use self.Q.add(...)
-        def addQ(self, Q):
-            self.Q.add(Q)
+#Not sure about the following methods   
+    def addQ(self, Q):
+        self.Q.add(Q)
 
-        def addR(self, R):
-            self.R.add(R)
+    def addR(self, reg):
+        self.R.append(reg)
 
-        def addTransition(self, t):
-            self.delta.add(t)
+    def addTransition(self, t):
+        self.delta.add(t)
 
-        def addI(self, I):
-            self.I.add(I)
+    def addI(self, I):
+        self.I.add(I)
 
-        def addF(self, F):
-            self.F.add(F)
-
-    """
+    def addF(self, F):
+        self.F.add(F)
 
 class DRsA(RsA):
     def __init__(self, Q, R, delta, I, F):
         RsA.__init__(self, Q, R, delta, I, F)
 
     def runWord(self, word):
-        print("This would do a deterministic run of word", word, "on this DRsA")
+        cnt = 0
+        c = ''
+        for i in self.I:
+            c = i
+            cnt += 1
+        if cnt > 1:
+            print("Multiple initial states in a deterministic automaton! Unspecified behavior.")
+        for i in word:
+            cnt = 0
+            for t in self.delta:
+                if t.orig == c and t.symbol == i[0] and self.guardTest(i[1], t.eqGuard, t.diseqGuard):
+                    #assuming there is only one such transition
+                    c = t.dest
+                    self.updateRegs(t.update)
+                    cnt += 1 
+                    break
+            if cnt == 0:
+                #no possible transition, run dies
+                self.clearRegs()
+                return False
+        if {c}.issubset(self.F):
+            self.clearRegs()
+            return True
+        else:
+            self.clearRegs()
+            return False
+                        
 
 
 class NRA(RsA):
---
+++ testRun.py
@@ -0,0 +1,27 @@
+import RsA as rsa
+
+dfa = rsa.DRsA({'q1', 'q2', 'q3'}, list(), set(), {'q1'}, {'q2'})
+dfa.delta.add(rsa.Transition('q1','0', list(), set(), set(),'q1'))
+dfa.addTransition(rsa.Transition('q1','1', list(), set(), set(),'q2'))
+dfa.addTransition(rsa.Transition('q2','0', list(), set(), set(),'q3'))
+dfa.addTransition(rsa.Transition('q2','1', list(), set(), set(),'q2'))
+dfa.addTransition(rsa.Transition('q3','0', list(), set(), set(),'q2'))
+dfa.addTransition(rsa.Transition('q3','1', list(), set(), set(),'q2'))
+
+
+word = (('1', 0), ('0', 0), ('0', 0))
+if dfa.runWord(word):
+    print("Accepted")
+else:
+    print("Rejected")
+
+
+drsa = rsa.DRsA({'q'}, [['r', set()]], set(), {'q'}, {'q'})
+drsa.addTransition(rsa.Transition('q','a', set(), {'r'}, set(),'q2'))
+
+word2 = [('a', 'a'), ('a', 'b'), ('a', 'c'), ('a', 'd')]
+
+if drsa.runWord(word2):
+    print("Accepted")
+else:
+    print("Rejected")
