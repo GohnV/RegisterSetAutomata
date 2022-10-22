6d537ec1c82db5bb75caf8496d6f84bc348e3ee3
JohnV <johnv@wenca.cz>
2022-10-22 11:54:48 +0200

    Added primitive regex parser

--- testRun.py
+++ testRun.py
@@ -1,7 +1,154 @@
 import RsA as rsa
 import graphviz
-
+#TODO predelat parser tabulku, iterace by mela mit prednost pred konkatenaci, mam to naopak
 #file to play with whatever I just finished to check basic functionality
+
+class Pair:
+    type = ''
+    data = ''
+    def __init__(self, type, data):
+        self.type = type
+        self.data = data
+    def createPair(self, symbol):
+        if symbol in '&|*()$':
+            self.type = symbol
+            self. data = ''
+        else:
+            self.type = 'i'
+            self.data = symbol
+
+class Pushdown:
+    data = []
+    def push(self, item):
+        for i in item.split():
+            self.data.append(i)
+    def top(self):
+        return self.data[-1]
+    def pop(self):
+        return self.data.pop(-1)
+    def topTerm(self):
+        pass
+
+
+
+def topTerm(list):
+    for i in range(1,len(list)+1):
+        if list[-i].type != 'E':
+            return list[-i]
+def rIndex(list, item):
+
+    for i in range(1,len(list)+1):
+
+        if list[-i].type == item.type:
+            return len(list) - i   
+    return -1       
+
+def strAfterInd(list, index):
+    str = ''
+    for i in range(index+1, len(list)):
+        if str == '':
+            str = list[i].type
+        else:
+            str = str + ' ' + list[i].type
+    return str
+
+def createTree(expr):
+    rules = ['E & E','E | E', 'E *', '( E )', 'i']
+    dict = {'&':0, '|':1, '*':2, '(':3, ')':4, 'i':5, '$': 6}
+    table = [['>', '>', '>', '<', '>', '<', '>'],
+             ['<', '>', '<', '<', '>', '<', '>'],
+             ['<', '>', '>', '' , '>', '' , '>'],
+             ['<', '<', '<', '<', '=', '<', '' ],
+             ['>', '>', '>', '' , '>', '' , '>'],
+             ['>', '>', '>', '' , '>', '' , '>'],
+             ['<', '<', '<', '<', '' , '<', '' ]]
+    i = 0
+    captCount = 0
+    pushdown = [Pair('$','')]
+    while True:
+        a = topTerm(pushdown)
+        ai = rIndex(pushdown, a)
+        
+        b = Pair('','')
+        b.createPair(expr[i])
+        '''
+        print('a = ', a.type,',', 'ai = ', ai, end = ' ')
+        print('b = ', b.type)
+        print("start:", end=' ')
+        for p in pushdown:
+            print(p.type, end='')
+        print()
+        '''    
+        #catch KeyError!!!!
+        tblSymbol = table[dict[a.type]][dict[b.type]]
+        #print("symbol:",tblSymbol)
+        if tblSymbol == '=':
+            pushdown.append(b)
+            i += 1
+        elif tblSymbol == '<':
+            pushdown.insert(ai+1, Pair('<', ''))
+            pushdown.append(b)
+            i += 1  
+        elif tblSymbol == '>':
+            ind = rIndex(pushdown, Pair('<', ''))
+            if ind != -1:
+                string = strAfterInd(pushdown, ind)
+                if string in rules:
+                    # Vyrobeni konkretniho malyho stromu
+                    #potom priradit strom do dat paru
+
+                    tree= rsa.SyntaxTree()
+
+                    if len(string) == 1:
+                        tree.children = []
+                        if pushdown[ind+1].data.isnumeric():
+                            tree.data = rsa.BACKREFCHAR + pushdown[ind+1].data
+                        elif pushdown[ind+1].data == '.':
+                            tree.data = rsa.ANYCHAR
+                        elif pushdown[ind+1].data == '@':
+                            captCount += 1
+                            tree.data = rsa.CAPTURECHAR + str(captCount)
+                        else:
+                            tree.data = pushdown[ind+1].data
+
+                    elif string[0] == '(':
+                        tree = pushdown[ind+2].data
+                    #Tohle asi predelat, je to humus:
+                    elif string[0] == 'E':
+                        if string[2] == '*':
+                            tree.data = rsa.ITERATION
+                            tree.children = [pushdown[ind+1].data]
+                        else:
+                            tree.children = [pushdown[ind+1].data, pushdown[ind+3].data]
+                            if string[2] == '&':
+                                tree.data = rsa.CONCATENATION
+                            elif string[2] == '|':
+                                tree.data = rsa.UNION
+                    #Odsataneni polozek ze zasobniku
+                    for j in range(ind, len(pushdown)):
+                        pushdown.pop(ind)
+                    pushdown.append(Pair('E', tree))
+                    #print('E ->', str)
+                else:
+                    print('chyba1')
+                    return -1
+            else:
+                print('chyba2')
+                return -1
+        else:
+            print('chyba3')
+            return -1
+        #print('i =', i,'expr[i] =', expr[i] )
+
+        '''
+        print('end:', end = ' ')
+        for p in pushdown:
+            print(p.type, end='')
+        print()
+        ''' 
+        if (b.type == '$' and len(pushdown) == 2):
+            if pushdown[0].type == '$' and pushdown[1].type == 'E':
+                return pushdown[1].data
 
 #draws the automaton into a pdf using graphviz
 def drawAutomaton(aut, name):
@@ -45,8 +192,20 @@ def drawSyntaxTree(tree, name):
     graph = graphviz.Digraph(name)
     createGraph(tree, graph, id)
     graph.render()
-    
 
+
+parsedTree = createTree('@&(.*)&;&(.*)&@&(.*)&;&(.*)&@&(.*)&3&2&1$')
+
+drawSyntaxTree(parsedTree, "parsedTree")
+id = rsa.Counter()
+parsedTree.createAutomaton(id)
+parsedAutomaton = parsedTree.automaton
+parsedAutomaton.removeEps()
+parsedAutomaton.removeUnreachable()
+
+drawAutomaton(parsedTree.automaton, "parsedAutomaton")
+
+'''
 dfa = rsa.DRsA({'q1', 'q2', 'q3'}, set(), set(), {'q1'}, {'q2'})
 dfa.delta.add(rsa.Transition('q1','0', set(), set(), {},'q1'))
 dfa.addTransition(rsa.Transition('q1','1', set(), set(), {},'q2'))
@@ -123,6 +282,43 @@ drawAutomaton(biggerTestTree.automaton, 'biggerTestAutomaton')
 
 drawSyntaxTree(biggerTestTree, 'biggerTestTree')
 
+exRegex = rsa.SyntaxTree()
+
+
+exReCon2 = rsa.SyntaxTree()
+exReCon2.data = rsa.CONCATENATION
+exReCon3 = rsa.SyntaxTree()
+
+exReCon3.data = rsa.CONCATENATION
+
+exReCon4 = rsa.SyntaxTree()
+exReCon4.data = rsa.CONCATENATION
+
+
+exReCapt1 = rsa.SyntaxTree()
+exReCapt1.data = rsa.CAPTURECHAR+'2'
+
+exReAnychar2 = rsa.SyntaxTree()
+exReAnychar2.data = rsa.ANYCHAR
+
+exReCon3.children = [exReAnychar2]
+
+
+exReCapt2 = rsa.SyntaxTree()
+exReCapt2.data = rsa.CAPTURECHAR+'2'
+exReCon3.children = [exReCapt2, exReCon4]
+
+exReIte1 = rsa.SyntaxTree()
+exReIte1.data = rsa.ITERATION
+exReAnychar1 = rsa.SyntaxTree()
+exReAnychar1.data = rsa.ANYCHAR
+exReIte1.children = [exReAnychar1]
+exReCon2.children = [exReIte1, exReCon3]
+exRegex.data = rsa.CONCATENATION
+exRegex.children = [exReCapt1, exReCon2]
+
+drawSyntaxTree(exRegex, 'exampleTree')
+
 print(testTree.automaton.Q)
 print(testTree.automaton.R)
 for t in testTree.automaton.delta:
@@ -149,5 +345,5 @@ if testDRSA.runWord(word3):
 else:
     print("Rejected")
 
-
+'''
 
