f6feabe18bd7eb5a67f6724afa9ea2449e9ef0c0
JohnV <johnv@wenca.cz>
2022-12-30 12:53:19 +0100

    Added scatterplot

--- plot_bench.sh
+++ plot_bench.sh
@@ -6,6 +6,8 @@ GREP='grep.txt'
 #python3 ./bench_my.py >$MY
 #python3 ./bench_grep.py >$GREP
 
+awk '{getline f1 < "grep.txt"; $2 = $2 OFS f1; print}' my_good.txt > scatter.txt #FIXME:file names and sizes
+
 gnuplot -persist <<-EOFMarker
     set terminal svg
     set output "$OUT"
@@ -18,4 +20,20 @@ gnuplot -persist <<-EOFMarker
     set grid
 
     plot "$MY" title "RsA implementation" with lines linestyle 1, "$GREP" title "grep implementation" with lines linestyle 2
+EOFMarker
+
+gnuplot -persist <<-EOFMarker
+    set terminal svg
+    set output "scatter.svg" #FIXME:    
+
+    set multiplot
+
+    set ylabel "grep time (s)" font ",18"
+    set xlabel "RsA time (s)" font ",18"
+    set xrange [:300]
+    set yrange [:300]
+    set size square
+    set logscale
+    set grid
+    plot "scatter.txt" using 2:4 title "", x title ""
 EOFMarker
